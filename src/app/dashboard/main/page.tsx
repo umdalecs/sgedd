4afbb6ca0c578eed6083c259da10ffbd5856b5e7
CESAR IVAN MEZA QUINONEZ <l22170727@culiacan.tecnm.mx>
2025-11-07 18:51:04 -0700

    1er version del header

---
+++ src/app/dashboard/main/page.tsx
@@ -0,0 +1,8 @@
+import Header from "@/components/header/Header";
+export default function Page() {
+  return (
+    <>
+        <Header rol="Docente"></Header>
+    </>
+  )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ export default function Home({} : PageProps<'/'>) {
       <Card className="w-lg">
         <CardContent>
            <div className="mb-6 text-center">
-            <Image src="/img/logo-sgedd.jpg" alt="SGEDD" width={250} height={250} className="mx-auto my-5"/>
+            <Image src="/img/logo-sgedd.png" alt="SGEDD" width={250} height={250} className="mx-auto my-5"/>
             <h1 className="text-foreground text-5xl mb-8">SGEDD</h1>
             <p className="text-sm text-muted-foreground mb-3 font-bold">Sistema Gestor al Estímulo del Desempeño Docente</p>
           </div>
