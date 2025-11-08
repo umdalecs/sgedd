87c9f225c006e70174f46f7ebfe045b2975b31dd
CESAR IVAN MEZA QUINONEZ <l22170727@culiacan.tecnm.mx>
2025-11-08 14:00:53 -0700

    Finalizacion de Inicio, anexo de tipo Usuario y mejoras en la vista y responsividad.

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,12 +1,30 @@
 import DashboardCards from "@/components/common/DashboardCard";
 import Header from "@/components/common/Header";
+import { Usuario, UsuarioDocente, asignarRol } from "@/types/usuario";
 
 export default function Page() {
+  const usuarioTest: Usuario ={
+    nombre : "Pedro",
+    apellido : "Sanchez",
+    correo: "pedro@example.com",
+    puesto: "Vinculacion",
+    rfc: "PEASXXX1231",
+    rol: asignarRol("Vinculacion"),
+  }
+  const usuarioTestD: UsuarioDocente ={
+    nombre : "Pedro",
+    apellido : "Sanchez",
+    correo: "pedro@example.com",
+    puesto: "Docente",
+    rfc: "PEASXXX1231",
+    estatusPlaza: "Activo",
+    rol: asignarRol("Docente"),
+  }
   return (
     <>
     <div className="min-h-70">
-      <Header rol="Docente"></Header>
-      <DashboardCards />
+      <Header rol={usuarioTestD.rol} />
+      <DashboardCards usuario={usuarioTestD}/>
     </div>
     </>
   )
--- src/components/common/DashboardCard.tsx
+++ src/components/common/DashboardCard.tsx
@@ -1,36 +1,50 @@
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Usuario, UsuarioDocente } from "@/types/usuario";
 
-export default function DashboardCards() {
+
+type Props = {
+  usuario: Usuario | UsuarioDocente;
+};
+
+export default function DashboardCards({ usuario }: Props) {
     return(
         <>
-        <div className="flex justify-center items-center h-2/3 mt-14">
-            <Card className="flex-col w-2/3 min-w-lg h-full py-0 gap-0 min-h-90">
+        <div className="flex justify-center items-center w-full py-22">
+            <Card className="flex-col w-1/2 min-w-lg h-full py-0 gap-0 min-h-90">
                 <CardHeader className="bg-primary text-primary-foreground rounded-t-xl py-3 text-center gap-0">
                     <CardTitle className="text-3xl font-bold">Perfil de Usuario</CardTitle>
                 </CardHeader>
                 <CardContent className="flex-1 flex items-center justify-center gap-0">
-                    <div className="flex w-full h-full px-10">
+                    <div className="flex w-full h-full">
                         <div className="w-1/3 flex items-center justify-center">
-                        <div className="w-32 h-32 bg-gray-200 rounded-full flex items-center justify-center text-gray-600">
+                        <div className="w-32 h-32 bg-sidebar-border rounded-full flex items-center justify-center">
                             
                         </div>
                         </div>
                         <div className="w-2/3 flex flex-col justify-center gap-8">
                         <div className="flex gap-2">
                             <p className="font-semibold w-1/3">Nombre:</p>
-                            <p className="w-2/3">Pedro Martinez del salsalsalsla</p>
+                            <p className="w-2/3">{usuario.nombre} {usuario.apellido}</p>
                         </div>
                         <div className="flex gap-2">
                             <p className="font-semibold w-1/3">Correo:</p>
-                            <p className="w-2/3">pedro@example.com</p>
+                            <p className="w-2/3">{usuario.correo}</p>
                         </div>
-                        <div className="flex gap-2">
+                        {(usuario.rol === "Docente" && "estatusPlaza" in usuario) &&(
+                            <div className="flex gap-2">
                             <p className="font-semibold w-1/3">Estatus plaza:</p>
-                            <p className="w-2/3">Activo</p>
-                        </div>
+                            <p className="w-2/3">{usuario.estatusPlaza}</p>
+                            </div>
+                        )}
+                        {(usuario.rol === "Generador" || usuario.rol === "Revisor") &&(
+                            <div className="flex gap-2">
+                            <p className="font-semibold w-1/3">Puesto:</p>
+                            <p className="w-2/3">{usuario.puesto}</p>
+                            </div>
+                        )}
                         <div className="flex gap-2">
                             <p className="font-semibold w-1/3">RFC:</p>
-                            <p className="w-2/3">PEMJ880101XXX</p>
+                            <p className="w-2/3">{usuario.rfc}</p>
                         </div>
                         </div>
                     </div>
