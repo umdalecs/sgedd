fb2e6f32c1595e2c5c5522bbd1ac9d7afcef17ab
César Iván Meza <cesarmezaqui@gmail.com>
2025-11-26 18:46:59 -0700

    Base para hacer los pdfs, instalaciones de paquetes extra (#21)

---
+++ src/app/dashboard/generaciones/test/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+import React from 'react'
+import BotonPDF from '../../../../components/botonPdf';
+
+export default function Page() {
+  return (
+    <div>
+        <h1>Generador de PDF</h1>
+        <BotonPDF />
+    </div>
+  )
+}
