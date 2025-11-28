e5afe127029febc4672db50f90f18ca93bf0d230
Alejandro Flores <alejandro.flores.alecs@gmail.com>
2025-11-28 02:20:57 -0700

    something?

--- src/components/generador/Generador.tsx
+++ src/components/generador/Generador.tsx
@@ -2,17 +2,11 @@
 import { EventoGeneracion } from "@/types/EventoGeneracion";
 import { Button } from "../ui/button";
 import { generarPdf } from "@/lib/actions/documents";
-import { redirect } from "next/navigation";
 
 export default function Generador({ evento }: { evento: EventoGeneracion }) {
 
-  // TODO: Generar los pdfs
   const handleClick = async (evento: EventoGeneracion) => {
-    const { data, error } = await generarPdf(evento);
-
-    if (error) {
-      redirect("/dashboard")
-    }
+    await generarPdf(evento);
   };
 
   return (
