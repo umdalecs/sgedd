91b893c527a728d57bc92ecbc494ce1c1367f964
Alejandro Flores <alejandro.flores.alecs@gmail.com>
2025-12-02 16:31:07 -0700

    cambios generador

--- src/app/dashboard/generaciones/viewer/[documentoId]/page.tsx
+++ src/app/dashboard/generaciones/viewer/[documentoId]/page.tsx
@@ -1,12 +1,12 @@
 "use server";
-import DocContent from "@/components/common/DocContent";
 import PDFViewerCard from "@/components/common/PDFViewerCard";
+import Firmar from "@/components/generador/Firmar";
 import { ReportaGenerador } from "@/components/generador/ReportaGenerador";
 import SolicitarFirmas from "@/components/generador/SolicitarFirmas";
 import { Button } from "@/components/ui/button";
 
 import { getDocumentByID } from "@/lib/actions/documents";
-import { CheckLine, FilePen } from "lucide-react";
+import { CheckLine } from "lucide-react";
 import Link from "next/link";
 import { redirect } from "next/navigation";
 
@@ -15,7 +15,7 @@ export default async function Page({
 }: PageProps<"/dashboard/generaciones/viewer/[documentoId]">) {
   const { documentoId } = await params;
 
-  const { data, error } = await getDocumentByID(documentoId);
+  const { data: documento, error } = await getDocumentByID(documentoId);
 
   if (error) {
     redirect("/dashboard");
@@ -28,23 +28,18 @@ export default async function Page({
           <div className="bg-sidebar-accent-foreground text-accent w-full text-center font-bold text-2xl rounded-2xl">
             Solicitante:
           </div>
-          <div className="font-bold">{data!.expediente!.docente_rfc}</div>
+          <div className="font-bold">{documento!.expediente!.docente_rfc}</div>
         </div>
         <div className="flex flex-col items-center w-1/4">
           <div className="bg-sidebar-accent-foreground text-accent w-full text-center font-bold text-2xl rounded-2xl">
             Estado:
           </div>
-          <div className="font-bold">{data?.estadoactual}</div>
+          <div className="font-bold">{documento?.estadoactual}</div>
         </div>
       </div>
-      <PDFViewerCard url={data!.rutaarchivo}>
+      <PDFViewerCard url={documento!.rutaarchivo}>
         <div className="flex flex-col w-full h-full items-center justify-around">
-          <Button className="flex w-3/4 h-15 text-2xl font-bold">
-            <div>Firmar</div>
-            <div>
-              <FilePen className="size-full" />
-            </div>
-          </Button>
+          <Firmar documento={documento!} />
           <SolicitarFirmas />
           <Button className="w-3/4 h-15 ">
             <Link
@@ -58,7 +53,7 @@ export default async function Page({
             </Link>
           </Button>
           <ReportaGenerador
-            path={`/dashboard/generaciones/report/${data!.documentoid}`}
+            path={`/dashboard/generaciones/report/${documento!.documentoid}`}
           />
         </div>
       </PDFViewerCard>
